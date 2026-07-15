002d8b6b8d29702198743429a6b8e767f1fb3086
mgalexandre <99873300+mgalexandre@users.noreply.github.com>
2026-07-15 05:51:44 +0100

    feat(validation): include error messages in ValidationFailed exception message (#2203)

--- src/Exceptions/ValidationFailed.php
+++ src/Exceptions/ValidationFailed.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Tempest\Validation\Exceptions;
 
 use Exception;
+use Tempest\Core\ProvidesContext;
 use Tempest\Validation\FailingRule;
 use Tempest\Validation\Internal\MessageRule;
 
-final class ValidationFailed extends Exception
+final class ValidationFailed extends Exception implements ProvidesContext
 {
     /**
      * @template TKey of array-key
@@ -23,10 +24,23 @@ final class ValidationFailed extends Exception
         private(set) array $errorMessages = [],
         private(set) ?string $targetClass = null,
     ) {
-        parent::__construct(match (true) {
+        $message = match (true) {
             is_null($subject) => 'Validation failed.',
             default => sprintf('Validation failed for %s.', is_object($subject) ? $subject::class : $subject),
-        });
+        };
+
+        foreach ($errorMessages as $field => $messages) {
+            $message .= sprintf('%s- %s: %s', PHP_EOL, $field, implode('; ', $messages));
+        }
+
+        parent::__construct($message);
+    }
+
+    public function context(): iterable
+    {
+        return array_filter([
+            'errors' => $this->errorMessages,
+        ]);
     }
 
     /**
